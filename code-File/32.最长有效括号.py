e2994b9ac9f93beba9e4d660934427fd2bb62fe6
snsun_lee <snsunlee123@gmail.com>
2019-05-27 09:12:38 +0800

    Update 32.最长有效括号.py

--- code-File/32.最长有效括号.py
+++ code-File/32.最长有效括号.py
@@ -15,17 +15,15 @@ class Solution:
                 res.append(i)
             if s[i] == "(":
                 stack.append(i)
-        return self.findLengthOfLCIS(res.sort())
-    def findLengthOfLCIS(self, nums: List[int]) -> int:
-        if not nums:
-            return 0
-        res=1
-        new=1
-        for i in range(len(nums)-1):
-            if nums[i]<nums[i+1]:
-                res+=1
-                new=max(res,new)
-                if i!=len(nums)-2 and nums[i+1]>=nums[i+2]:
-                    res=1
-        return new
-
+        res.sort()
+        #print(res)
+        i = 0
+        ans = 0
+        n = len(res)
+        while i < n:
+            j = i
+            while j < n - 1 and res[j + 1] == res[j] + 1:
+                j += 1
+            ans = max(ans, j - i + 1)
+            i = j + 1
+        return ans
