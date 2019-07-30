0b95be1e1c354e04864c25bde4f133e806e33cd9
李欣尚 <snsunlee123@gmail.com>
2019-07-30 18:00:11 +0800

    Update 70.爬楼梯.py

--- code-File/70.爬楼梯.py
+++ code-File/70.爬楼梯.py
@@ -4,6 +4,7 @@
 # [70] 爬楼梯
 #
 class Solution:
+    ####利用记忆化存储来减少重复计算
     def climbStairs(self, n: int) -> int:
         if n<=2:
             return n
@@ -13,4 +14,15 @@ class Solution:
             first=second
             second=third
         return second
+    ####思路一致
+     def climbStairs1(self, n: int) -> int:
+        cash=[1,1]
+        if n==0 or n==1:
+            return 1
+        i=2
+        while i<n:
+            cash.append(cash[i-1] + cash[i-2])
+            i+=1
+        cash.append(cash[i-1] + cash[i-2])
+        return cash[i]
 
