85e620d2359b2044eef29016d114e60395e88a1c
snsunlee <snsunlee123@gmail.com>
2019-08-06 12:16:11 +0800

    add 95

---
+++ code-File/95.不同的二叉搜索树-ii.py
@@ -0,0 +1,75 @@
+#
+# @lc app=leetcode.cn id=95 lang=python3
+#
+# [95] 不同的二叉搜索树 II
+#
+# https://leetcode-cn.com/problems/unique-binary-search-trees-ii/description/
+#
+# algorithms
+# Medium (58.22%)
+# Likes:    166
+# Dislikes: 0
+# Total Accepted:    8.5K
+# Total Submissions: 14.5K
+# Testcase Example:  '3'
+#
+# 给定一个整数 n，生成所有由 1 ... n 为节点所组成的二叉搜索树。
+# 
+# 示例:
+# 
+# 输入: 3
+# 输出:
+# [
+# [1,null,3,2],
+# [3,2,null,1],
+# [3,1,null,null,2],
+# [2,1,3],
+# [1,null,2,null,3]
+# ]
+# 解释:
+# 以上的输出对应以下 5 种不同结构的二叉搜索树：
+# 
+# ⁠  1         3     3      2      1
+# ⁠   \       /     /      / \      \
+# ⁠    3     2     1      1   3      2
+# ⁠   /     /       \                 \
+# ⁠  2     1         2                 3
+# 
+# 
+#
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+class Solution:
+    def generateTrees(self, n):
+        """
+        :type n: int
+        :rtype: List[TreeNode]
+        """
+        def generate_trees(start, end):
+            if start > end:
+                return [None,]
+            
+            all_trees = []
+            for i in range(start, end + 1):  # pick up a root
+                # all possible left subtrees if i is choosen to be a root
+                left_trees = generate_trees(start, i - 1)
+                
+                # all possible right subtrees if i is choosen to be a root
+                right_trees = generate_trees(i + 1, end)
+                
+                # connect left and right subtrees to the root i
+                for l in left_trees:
+                    for r in right_trees:
+                        current_tree = TreeNode(i)
+                        current_tree.left = l
+                        current_tree.right = r
+                        all_trees.append(current_tree)
+            
+            return all_trees
+        
+        return generate_trees(1, n) if n else []
+
